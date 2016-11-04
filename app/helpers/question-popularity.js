20b379dda2d554367d091cf9cd05de5149521b6f
Jena SanCartier <jenasancartier@gmail.com>
2016-11-04 15:57:26 -0700

    add more answers to questions to activate helper

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,7 +4,7 @@ export function questionPopularity(params) {
   var question = params[0];
 
   if(question.get('answers').get('length') >= 5) {
-    return Ember.String.htmlSafe('<img src="./../images/top-games-star.png">');
+    return Ember.String.htmlSafe('<img src="./../images/big-bulb.png">');
   }
 }
 
