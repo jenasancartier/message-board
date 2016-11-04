850e23d51559f6cef92bb39d52eeb3e1e9f72bb8
Jena SanCartier <jenasancartier@gmail.com>
2016-11-04 08:47:57 -0700

    add myFavorites service, add a component to display the favorite questions, and the route to display the component

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  myFavorites: Ember.inject.service(),
   updateQuestionForm: false,
+
+  actions: {
+    addToFavorites(favorite) {
+      this.get('myFavorites').add(favorite);
+    },
+  }
 });
